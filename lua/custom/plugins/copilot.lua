973dbb7afe3c3d388116be54f3ee6030384c23b4
mPyKen <mpyken.gh@gmail.com>
2024-02-09 20:29:42 +0900

    Disable copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,33 +1,34 @@
-return {
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        enabled = false,
-        auto_trigger = false,
-        keymap = {
-          accept = "<Tab>",
-          close = "<Esc>",
-          next = "<C-J>",
-          prev = "<C-K>",
-          select = "<CR>",
-          dismiss = "<C-X>",
-        },
-      },
-      panel = {
-        enabled = false,
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "zbirenbaum/copilot.lua",
-    },
-    config = true,
-  },
-}
+return {}
+-- return {
+--   {
+--     "zbirenbaum/copilot.lua",
+--     cmd = "Copilot",
+--     build = ":Copilot auth",
+--     event = "InsertEnter",
+--     opts = {
+--       suggestion = {
+--         enabled = false,
+--         auto_trigger = false,
+--         keymap = {
+--           accept = "<Tab>",
+--           close = "<Esc>",
+--           next = "<C-J>",
+--           prev = "<C-K>",
+--           select = "<CR>",
+--           dismiss = "<C-X>",
+--         },
+--       },
+--       panel = {
+--         enabled = false,
+--       },
+--     },
+--   },
+--   {
+--     "zbirenbaum/copilot-cmp",
+--     dependencies = {
+--       "hrsh7th/nvim-cmp",
+--       "zbirenbaum/copilot.lua",
+--     },
+--     config = true,
+--   },
+-- }
