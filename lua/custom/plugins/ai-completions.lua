a7ce2964bf653ea50ac2bef3630bb7faa2b590a9
mPyKen <mpyken.gh@gmail.com>
2024-08-03 22:54:45 +0900

    Use codeium

---
+++ lua/custom/plugins/ai-completions.lua
@@ -0,0 +1,20 @@
+return {
+  'Exafunction/codeium.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    require('codeium').setup {}
+  end,
+}
+
+--- lua
+-- vim.g.tabby_keybinding_accept = '<Tab>'
+-- vim.g.tabby_keybinding_trigger_or_dismiss = '<C-\\>'
+
+-- return {
+--   {
+--     "TabbyML/vim-tabby",
+--   },
+-- }
