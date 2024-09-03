3d160e48b0422c9720a16edf0b275d86a6900642
mPyKen <mpyken.gh@gmail.com>
2024-09-03 22:12:50 +0900

    Add persisted

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,28 +5,15 @@ local command = vim.api.nvim_create_user_command
 local PyKenGroup = augroup('PyKen', {})
 
 function R(name)
-  require("plenary.reload").reload_module(name)
-end
-
-local function write_session()
-  if vim.fn.empty(vim.v.this_session) == 0 then
-    vim.cmd("mks! " .. vim.v.this_session)
-  end
+  require('plenary.reload').reload_module(name)
 end
 
 command('Q', 'qa', {})
 
-autocmd({ "VimLeave" }, {
+autocmd({ 'BufWritePre' }, {
   group = PyKenGroup,
-  desc = "Save active sessions on exit",
-  pattern = "*",
-  callback = write_session,
-})
-
-autocmd({ "BufWritePre" }, {
-  group = PyKenGroup,
-  desc = "Remove trailing white spaces before writing a buffer",
-  pattern = "*",
+  desc = 'Remove trailing white spaces before writing a buffer',
+  pattern = '*',
   command = [[%s/\s\+$//e]],
 })
 
