2a1fbc4e40aa35e83caf9ad545951d864e87b893
mPyKen <mpyken.gh@gmail.com>
2025-01-03 12:19:44 +0900

    Fix terraform commenting and fix saving session during git commit messages

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -65,4 +65,13 @@ end
 
 set_wildignore_from_gitignore()
 
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('FixTerraformCommentString', { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = '# %s'
+  end,
+  pattern = { 'terraform', 'hcl' },
+})
+
 return {}
