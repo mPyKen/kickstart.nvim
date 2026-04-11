7d457b662adf51ed907ed3f80e8e548da8817e24
mPyKen <mpyken.gh@gmail.com>
2026-04-11 11:21:20 +0900

    Disable clipboard sync Add FormatDisable and FormatEnable commands

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -9,6 +9,24 @@ vim.keymap.set('n', '<leader>tw', ':set wrap!<CR>', { desc = 'Toggle Wrap' })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+vim.api.nvim_create_user_command('FormatDisable', function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = 'Disable autoformat-on-save',
+  bang = true,
+})
+vim.api.nvim_create_user_command('FormatEnable', function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = 'Re-enable autoformat-on-save',
+})
+
 local move_lines = function(count, is_down)
   -- switch to normal mode
   local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
